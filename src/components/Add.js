25d29fdb36f7e122057d71d0b4ac884258031e93
Edgar Ramos Luna <edgarramoslun333@gmail.com>
2023-01-12 01:15:50 -0600

    Rebuilding proyect for errors on local enviroment

---
+++ src/components/Add.js
@@ -0,0 +1,41 @@
+import React, { useContext, useState } from "react";
+import { SocketContext } from "../context/SocketContext";
+//import { useSocket } from "../hooks/useSocket";
+
+const Add = () => {
+    const {socket} = useContext(SocketContext);
+    const [valor, setValor] = useState('');
+  //  const {socket} = useSocket('http://localhost:8080');
+    /*const crear = (name) =>{
+        socket.emit('crear-banda', name);
+    }*/
+    const onSubmit = (e) =>{
+        e.preventDefault();
+        console.log(valor);
+        if(valor.trim().length > 0){
+            //TODO: llamar la funcion para emitir el evneto
+            //crear(valor);
+            //socket.emit('crear-banda', {name:valor});
+            socket.emit('crear-banda', {name:valor});
+            setValor('');
+        }
+    }
+  return (
+    <>
+      <h3>Agregar Candidato</h3>
+      <form onSubmit={onSubmit}>
+        <input
+          className="form-control"
+          type=""
+          name=""
+          value={valor}
+          placeholder="Nuevo candidato"
+          onChange={(e) => setValor(e.target.value)}
+        />
+      </form>
+      <button>Reiniciar Votaciones</button>
+    </>
+  );
+};
+
+export default Add;
